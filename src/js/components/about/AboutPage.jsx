42a6e5ceea8855e33547d452d2f748ce63b80d3a
lchauche <loic.chaucheprat@cnp.fr>
2018-12-21 16:56:50 +0100

    ajout composant About + routing

---
+++ src/js/components/about/AboutPage.jsx
@@ -0,0 +1,15 @@
+"use strict"
+
+var React = require("react");
+var ReactDOM = require("react-dom");
+var createClass = require("create-react-class");
+
+var About = createClass({
+  render: function(){
+    return (
+      <div>About page under construction...</div>
+    );
+  }
+});
+
+module.exports = About;
